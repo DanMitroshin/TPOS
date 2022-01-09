cc559806ee93a371c1bad29c205e78a44ef3eea2
DanMitroshin <36602715+DanMitroshin@users.noreply.github.com>
2022-01-09 21:35:52 +0300

    Update file_cron.py

--- file_cron.py
+++ file_cron.py
@@ -2,10 +2,17 @@
 
 from crontab import CronTab
 
-if __name__ == '__main__':
+
+CRON_COMMAND = 'sed -i "s/is .*$/is $(($(ps -o etimes= -p $(cat /var/run/nginx.pid)) / 60)) minutes/" /opt/service_state'
+
+
+def start_change_cron():
     cron = CronTab(user='root')
     cron.remove_all()
-    write_job = cron.new(command='sed -i "s/is .*$/is $(($(ps -o etimes= -p $(cat /var/run/nginx.pid)) / 60)) minutes/" /opt/service_state', comment='update file from cron')
+    write_job = cron.new(command=CRON_COMMAND)
     write_job.minute.every(1)
     cron.write()
-    print('|> Start cron...')
+
+
+if __name__ == '__main__':
+    start_change_cron()
