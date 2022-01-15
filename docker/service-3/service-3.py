1b774c936fd7a68ae7b254ed3e591f8431643e52
DanMitroshin <36602715+DanMitroshin@users.noreply.github.com>
2022-01-16 01:05:19 +0300

    Update service-3.py

--- docker/service-3/service-3.py
+++ docker/service-3/service-3.py
@@ -1,17 +1,12 @@
-from sqlalchemy import create_engine, orm
-
 from flask import Flask, request
+from sqlalchemy import create_engine, orm
 from dataproduct.data import Product
 
 
 app = Flask(__name__)
+
 engine = create_engine('mysql+pymysql://root:@database:3306/db', pool_recycle=3600)
 session = orm.Session(engine)
-
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return 'Not found. Error 404\n', 404
 
 
 @app.route('/health')
@@ -19,12 +14,17 @@ def health():
     return 'ok\n', 200
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return 'Not found. Error 404\n', 404
+
+
 @app.route('/')
 def handler():
     id_product = request.args.get('id')
     if id_product is None:
-        return "Not provide product id", 500
+        return "Not provide product id\n", 500
     product = session.query(Product).get({'id': id_product})
     if product is None:
-        return "Unreal product id", 500
+        return "Unreal product id\n", 500
     return f"Product: {product.name} - {product.cost} RUB\n", 200
