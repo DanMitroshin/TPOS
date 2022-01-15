43ca88006415df813c48f2a3020714cfee8dea3e
DanMitroshin <36602715+DanMitroshin@users.noreply.github.com>
2022-01-16 00:49:02 +0300

    Update service-2.py

--- docker/service-2/service-2.py
+++ docker/service-2/service-2.py
@@ -5,21 +5,29 @@ from sqlalchemy import create_engine, orm
 
 from dataproduct.data import Base, Product
 
+def check_fill():
+    print('|>>>>> CHECK ROWS IN DB:')
+    rows = list(map(lambda p: f'id {p.id}. [name] {p.name}, [cost] {p.cost}', session.query(Product).all()))
+    for row in rows:
+        print(row)
+#     for product in session.query(Product).all():
+#         print(f'id={product.id}. name="{product.name}", cost={product.cost}')
 
-if __name__ == '__main__':
+def fill_db():
     engine = create_engine('mysql+pymysql://root:@database:3306/db')
     session = orm.Session(engine)
     Base.metadata.create_all(engine)
     print("|>>>> START READ CSV")
 
-    df = pd.read_csv('/data/data.csv')
+    df = pd.read_csv('dbdata/data.csv')
     print("|>>>> SUCCESS READ CSV")
     for id_row, row in df.iterrows():
-        print(row)
+        # print(row)
         product = Product(id=id_row + 1, name=row["name"], cost=row["cost"])
         session.add(product)
     session.commit()
+    print("|>>>> END FILL")
 
-    print('|>>>>> Rows added:')
-    for product in session.query(Product).all():
-        print(f' - id={product.id}, name="{product.name}", cost={product.cost}')
+if __name__ == '__main__':
+    fill_db()
+    check_fill()
