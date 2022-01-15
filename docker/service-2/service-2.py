622d6b9fe75f549f1f1f91f4b8cb4bc51a2b21b1
DanMitroshin <36602715+DanMitroshin@users.noreply.github.com>
2022-01-16 01:03:58 +0300

    Update service-2.py

--- docker/service-2/service-2.py
+++ docker/service-2/service-2.py
@@ -1,29 +1,26 @@
-from sys import stderr
-
 import pandas as pd
 from sqlalchemy import create_engine, orm
-
 from dataproduct.data import Base, Product
+
 
 def check_fill(session):
     print('|>>>>> CHECK ROWS IN DB:')
     rows = list(map(lambda p: f'id {p.id}. [name] {p.name}, [cost] {p.cost}', session.query(Product).all()))
     for row in rows:
         print(row)
-#     for product in session.query(Product).all():
-#         print(f'id={product.id}. name="{product.name}", cost={product.cost}')
+
 
 def fill_db(session):
     print("|>>>> START READ CSV")
-
     df = pd.read_csv('dbdata/data.csv')
     print("|>>>> SUCCESS READ CSV")
+    
     for id_row, row in df.iterrows():
         # print(row)
-        product = Product(id=id_row + 1, name=row["name"], cost=row["cost"])
-        session.add(product)
+        session.add(Product(id=id_row + 1, name=row["name"], cost=row["cost"]))
     session.commit()
-    print("|>>>> END FILL")
+    print("|>>>> END FILL DB")
+
 
 if __name__ == '__main__':
     engine = create_engine('mysql+pymysql://root:@database:3306/db')
@@ -32,3 +29,4 @@ if __name__ == '__main__':
     
     fill_db(session)
     check_fill(session)
+    print("|>>>> END OF SERVICE-2")
