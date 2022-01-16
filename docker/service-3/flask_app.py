6b0d3f442a1573d6568b31a60d3399025a83e4f1
DanMitroshin <36602715+DanMitroshin@users.noreply.github.com>
2022-01-16 11:35:10 +0300

    Update flask_app.py

--- docker/service-3/flask_app.py
+++ docker/service-3/flask_app.py
@@ -5,22 +5,22 @@ from dataproduct.data import Product
 
 app = Flask(__name__)
 
-engine = create_engine('mysql+pymysql://root:@database:3306/products_db')
+engine = create_engine('mysql+pymysql://root:@database:10994/products_db')
 session = orm.Session(engine)
 
 
 @app.route('/health')
-def health():
+def get_health():
     return 'ok\n', 200
 
 
 @app.errorhandler(404)
-def page_not_found(e):
+def not_found():
     return 'Not found. Error 404\n', 404
 
 
 @app.route('/')
-def handler():
+def get_product():
     id_product = request.args.get('id')
     if id_product is None:
         return "Not provide product id\n", 500
